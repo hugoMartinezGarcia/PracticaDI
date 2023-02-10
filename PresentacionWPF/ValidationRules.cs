4f8080a47601fa78c46ba4cfd896a99a64fb5c06
Hugo Martinez <hugokyoku@hotmail.com>
2023-02-10 22:30:20 +0100

    - Validation Rules en UCEMpleado

---
+++ PresentacionWPF/ValidationRules.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PresentacionWPF
+{
+    public class ValidationRules : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                // Campo obligatorio
+                if (((string)value).Length <= 0)
+                    return new ValidationResult(false, "El campo es obligatorio");
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, e.Message);
+            }
+
+            if ((((string)value).Length < Min) || (((string)value).Length > Max))
+            {
+                return new ValidationResult(false,
+                    "La longitud del campo debe de estar comprendida entre : " + Min + " y " + Max + ".");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
